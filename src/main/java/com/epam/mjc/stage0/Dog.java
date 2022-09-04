f5afc9513e8c1737caf05b87e8177415cf9631ae
monsterJohan <nikfil97@gmail.com>
2022-09-04 21:26:06 +0200

    Implemented Task1

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,10 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
-
+public class Dog extends Animal{
+    public Dog() {
+        super();
+        this.setColor("brown");
+        this.setNumberOfPaws(4);
+        this.setHasFur(true);
+    }
 }
